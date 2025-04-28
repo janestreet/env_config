d89486ed9da387c35eded562a550130a6a805c95
public-release <as-public-release@janestreet.com>
2025-04-28 19:35:05 +0100

    v0.18~preview.130.26+1192

--- src/load_from_environment.mli
+++ src/load_from_environment.mli
@@ -6,7 +6,7 @@ module Make (C : Configuration_intf.S) : sig
   (** [from_env_exn ~default] will:
       + return [default] if [C.environment_variable] is not set
       + raises if [C.environment_variable] can't be parsed into a [C.t] or
-      [C.Environment_override.t] *)
+        [C.Environment_override.t] *)
   val from_env_exn : default_config:C.t Or_error.t -> C.t
 
   val from_env_result : default_config:C.t Or_error.t -> (C.t, Could_not_load.t) Result.t
@@ -14,8 +14,8 @@ end
 
 module Make_overridable (C : Configuration_intf.Overridable) : sig
   (** [from_env_exn ~default] will do everything that [Required.from_env_exn] will do,
-      except that it will return [t] if [C.environment_variable] deserializes to [t] via
-      a [C.Environment_override.t]. *)
+      except that it will return [t] if [C.environment_variable] deserializes to [t] via a
+      [C.Environment_override.t]. *)
   val from_env_exn : default_config:C.t Or_error.t -> C.t
 
   val from_env_result : default_config:C.t Or_error.t -> (C.t, Could_not_load.t) Result.t
